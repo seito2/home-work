51ced50942a8f85baa3393e048df1565d00fe793
seito2 <seito1201@gmail.com>
2023-08-27 05:55:41 +0900

    Fix

--- api-example/src/main.rs
+++ api-example/src/main.rs
@@ -6,24 +6,21 @@ use lambda_http::{run, service_fn, Body, Error, Request, RequestExt, Response};
 /// - https://github.com/awslabs/aws-lambda-rust-runtime/tree/main/examples
 async fn function_handler(event: Request) -> Result<Response<Body>, Error> {
     // Extract some useful information from the request
-    let who = event
-        .query_string_parameters_ref()
-        .and_then(|params| params.first("name"))
-        .unwrap_or("world");
-    let message = format!("Hello {who}, this is an AWS Lambda HTTP request");
 
     // Return something that implements IntoResponse.
     // It will be serialized to the right response event automatically by the runtime
     let resp = Response::builder()
         .status(200)
-        .header("content-type", "text/html")
-        .body(message.into())
+        .header("content-type", "application/json")
+        .body("test".into())
         .map_err(Box::new)?;
     Ok(resp)
 }
 
 #[tokio::main]
 async fn main() -> Result<(), Error> {
+    tracing::info!("Initializing lambda function(Cold Start)...");
+
     tracing_subscriber::fmt()
         .with_max_level(tracing::Level::INFO)
         // disable printing the name of the module in every log line.
